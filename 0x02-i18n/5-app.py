3d9974a43338a497b80dd8f4b279d73518f15f0f
KelvinoKing <kelvinoking@outlook.com>
2024-04-10 19:13:36 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -56,7 +56,7 @@ def before_request():
         g.user = user
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """ Get locale """
     user = get_user()
@@ -67,7 +67,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', strict_slashes=False)
